62790ed4afa73e235a506c0c3a1da94b7aab934f
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2023-01-10 00:51:34 -0600

    0.3

--- src/base/rules/filenames.js
+++ src/base/rules/filenames.js
@@ -3,5 +3,5 @@ module.exports = {
   "filenames/no-index": "off",
   "filenames/match-exported": "off",
   // ==== ERRORS ====
-  "filenames/match-regex": ["error", "kebab"],
+  "filenames/match-regex": ["error", "^[a-z-]+(?:\.[a-z-]+)*$", true],
 };
